f3c704c4b1c6da0f6a6aeff3e6f570e77f99dc9d
Jofred Gil N. Yacon <yaconjofredgil@gmail.com>
2026-05-22 00:19:41 +0800

    Migrate to Tailwind v4 and fix font styling

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Poppins', 'sans-serif'], 
+      },
+      colors: {
+        maroon: {
+          DEFAULT: '#800000',
+          dark: '#4a0000'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
